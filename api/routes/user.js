8d13acb39df8a9c9cae4e0916785465b656263d9
UIProf <cis.roshan27ag@gmail.com>
2020-05-24 01:50:40 +1000

    add jsonwebtoken

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcript = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -53,23 +54,34 @@ router.post("/login", (req, res) => {
                 return res.status(404).json({
                     message: 'Auth failed'
                 });
-            }else {
-                bcript.compare(req.body.password, user[0].password, (err, result) => {
-                    if (err) {
-                        return res.status(401).json({
-                            message: 'Auth failed'
-                        });
-                    }
-                    if (result) {
-                        return res.status(200).json({
-                            message: 'Auth Successful'
-                        });
-                    }
-                    res.status(401).json({
-                        message: 'Auth failed'
-                    })
-                })
             }
+            bcript.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: 'Auth Successful',
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                })
+            })
+            
         })
         .catch(err => {
             console.log(err);
